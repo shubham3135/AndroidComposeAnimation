c340355fe6dcbb92d096dfa289e03d3fa2552e69
Shubham <shubhamkumarwinner@gmail.com>
2021-07-11 12:04:25 +0530

    Adding starter files

--- app/src/main/java/com/shubhamkumarwinner/composeanimation/MainActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/composeanimation/MainActivity.kt
@@ -3,36 +3,16 @@ package com.shubhamkumarwinner.composeanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.shubhamkumarwinner.composeanimation.ui.theme.ComposeAnimationTheme
+import com.shubhamkumarwinner.composeanimation.ui.home.Home
+import com.shubhamkumarwinner.composeanimation.ui.theme.AnimationCodelabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeAnimationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            AnimationCodelabTheme {
+                Home()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeAnimationTheme {
-        Greeting("Android")
     }
 }
